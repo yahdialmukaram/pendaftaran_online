ef8fd0717a3750948d5447b37bc8a750b7260f12
yahdi almukaram <yahdialmukaram03@gmail.com>
2021-07-21 13:23:01 +0700

    db

--- database/p_online.sql
+++ database/p_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 18, 2021 at 07:00 PM
+-- Generation Time: Jul 21, 2021 at 06:21 AM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.19
 
@@ -40,7 +40,7 @@ CREATE TABLE `table_jadwal` (
 --
 
 INSERT INTO `table_jadwal` (`id_jadwal`, `jadwal`, `kouta`, `kouta_terisi`) VALUES
-(21, '07/18/2021 7:36 PM sampai 07/15/2021 7:36 PM', '30', '4');
+(21, '07/18/2021 7:36 PM sampai 07/15/2021 7:36 PM', '100', '32');
 
 -- --------------------------------------------------------
 
@@ -54,17 +54,19 @@ CREATE TABLE `table_nilai` (
   `qiroah` varchar(255) NOT NULL,
   `wawancara_ortu` varchar(255) NOT NULL,
   `paquyuban` varchar(255) NOT NULL,
-  `nilai_akhir` varchar(255) NOT NULL
+  `nilai_akhir` varchar(255) NOT NULL,
+  `rangking` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `table_nilai`
 --
 
-INSERT INTO `table_nilai` (`id_nilai`, `id_user`, `qiroah`, `wawancara_ortu`, `paquyuban`, `nilai_akhir`) VALUES
-(17, 22, '0', '0', '0', '0'),
-(18, 21, '40', '55', '44', '46.333333333333336'),
-(19, 24, '33', '23', '33', '29.666666666666668');
+INSERT INTO `table_nilai` (`id_nilai`, `id_user`, `qiroah`, `wawancara_ortu`, `paquyuban`, `nilai_akhir`, `rangking`) VALUES
+(18, 21, '40', '55', '44', '46.333333333333336', ''),
+(20, 19, '60', '77', '78', '71.66666666666667', ''),
+(21, 20, '70', '77', '88', '78.33333333333333', ''),
+(26, 13, '0', '0', '0', '0', '');
 
 -- --------------------------------------------------------
 
@@ -79,13 +81,6 @@ CREATE TABLE `table_saran` (
   `subject` varchar(200) NOT NULL,
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `table_saran`
---
-
-INSERT INTO `table_saran` (`id_saran`, `nama`, `email`, `subject`, `message`) VALUES
-(2, 'yahdi almukaram', 'yahdialmukaram03@gmail.com', 'masalah login', 'ini sngat bagus untuk di kembangkan');
 
 -- --------------------------------------------------------
 
@@ -107,9 +102,10 @@ CREATE TABLE `table_sekolah` (
 --
 
 INSERT INTO `table_sekolah` (`id_sekolah`, `id_user`, `id_jadwal`, `nama_sekolah`, `sk_domisili`, `akta_kelahiran`) VALUES
-(32, '22', '21', 'SDIT Qurrata A\'yun 2 Lintau', '5f895146445f50da93013122cbb1a1d1.pdf', '2a02ed8f9059bf4731e1b97c0ff25957.pdf'),
 (33, '21', '21', 'SDIT Qurrata A\'yun Batusangkar', 'afb164db37eeb0a0bb630c52e286fec6.pdf', '0ac243fc43dc376d9c6996aea731031c.pdf'),
-(34, '24', '21', 'SDIT Qurrata A\'yun Batusangkar', '1425dbab4f4ab3593e59bac64bd9b6e6.pdf', 'a83236507718b65e2b1cb42cb9c2ca33.pdf');
+(35, '19', '21', 'SDIT Qurrata A\'yun 3 Batusangkar', 'd022189bd7414bd36d344bf483453535.pdf', 'b195f67487c6210451d39f71a1a20648.pdf'),
+(40, '20', '21', 'SDIT Qurrata A\'yun 2 Lintau', 'adf3786a6b77ebdd2a5549e72269a9fa.pdf', '0e35bc493acc26fe18ca1267153dcc08.pdf'),
+(62, '13', '21', 'SDIT Qurrata A\'yun Batusangkar', '9f23273697d1c3798ccaba5f26aef69a.pdf', 'fc4fb675593433e62262fd6d75b8c9ea.pdf');
 
 -- --------------------------------------------------------
 
@@ -161,8 +157,8 @@ CREATE TABLE `table_siswa` (
 INSERT INTO `table_siswa` (`id_siswa`, `id_user`, `no_registrasi`, `nik`, `nama`, `alamat`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `anak_ke`, `jumlah_keluarga`, `bahasa_dirumah`, `jarak_tempuh`, `golongan_darah`, `keadaan_fisik`, `penyakit`, `penyakit_diderita`, `bakat`, `hafalan`, `kemampuan_baca`, `kewarganegaraan`, `nama_ayah`, `no_hp_ortu`, `agama`, `status_perkawinan`, `alamat_orang_tua`, `status_rumah`, `pendidikan_ayah`, `pekerjaan_ayah`, `penghasilan_ayah`, `nama_ibu`, `pendidikan_ibu`, `pekerjaan_ibu`, `penghasilan_ibu`) VALUES
 (24, 19, '510072021001', '1771070307960001', 'sri', 'padang', 'padang panjang', '1996-07-03', 'Perempuan', '1', '2', 'minang', '1-2 km', 'a', 'normal', 'tidak ada', 'tidak ada', 'lomba tikat provinsi', '2 juz', 'fasih', 'Indonesia', 'kaka', '082169074845', 'Islam', 'Menikah', 'padang', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 1000.000.00 perbulan', 'nadia', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan'),
 (25, 21, '510072021002', '1771070307960002', 'nadia', 'malalo', 'padang panjang', '13.10.2020', 'Perempuan', '6', '3', 'indinesia', '5-6 km', 'o', 'normal', 'tidak ada', 'tidak ada', 'lomba tikat provinsi', '2 juz', 'fasih', 'Indonesia', 'arjalis', '082169074845', 'Islam', 'Menikah', 'padang', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 500.000.00 perbulan', 'kaka', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan'),
-(26, 24, '510072021003', '1771070307960003', 'mama', 'padang', 'padang', '16.10.2020', 'Laki Laki', '4', '3', 'indinesia', '1-2 km', 'a', 'normal', 'tidak ada', 'tidak ada', 'lomba tikat provinsi', '2 juz', 'fasih', 'Indonesia', 'paralis', '082169074845', 'Islam', 'Menikah', 'padang', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 500.000.00 perbulan', 'nadia', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan'),
-(27, 22, '511072021004', '1771070307960004', 'nana', 'padang', 'padang panjang', '1999-07-08', 'Perempuan', '2', '2', 'inggris', '1-2 km', 'o', 'non disabilitas', 'tidak ada', 'tidak ada', 'lomba tikat provinsi', '2 juz', 'fasih', 'Indonesia', 'paralis', '082169074845', 'Islam', 'Menikah', 'padang', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 500.000.00 perbulan', 'nadia', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan');
+(28, 20, '519072021005', '1304041208150022', 'anggi', 'padang', 'mama', '1996-07-03', 'Laki Laki', '1', '1', 'minang', '1-2 km', 'a', 'non disabilitas', 'tidak ada', 'tidak ada', 'lomba tikat provinsi', '3 juz', 'fasih', 'Indonesia', 'angga', '082169074845', 'Islam', 'Menikah', 'padang', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 500.000.00 perbulan', 'wewe', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan'),
+(29, 13, '520072021006', '1304041208150012', 'almukaram', '', '', '', 'Laki Laki', '1', '1', 'minang', '1-2 km', 'a', 'non disabilitas', '', '', '', '', '', '', '', '', 'Islam', 'Menikah', '', 'Kontrakan', 'SD', 'Tni/Polri', 'Rp. 500.000.00 perbulan', '', 'SD', 'Pedagang', 'Rp. 500.000.00 perbulan');
 
 -- --------------------------------------------------------
 
@@ -189,9 +185,8 @@ INSERT INTO `table_user` (`id_user`, `username`, `email`, `password`, `waktu`, `
 (19, 'sri', 'sri@gmail.com', 'd1565ebd8247bbb01472f80e24ad29b6', '09-07-2021, 16:48:54', 'siswa'),
 (20, 'anggi', 'anggi@gmail.com', '4a283e1f5eb8628c8631718fe87f5917', '10-07-2021, 01:39:53', 'siswa'),
 (21, 'nadia', 'nadia@gmail.com', 'a2e8cea3392da09d1d31be3fca68efed', '10-07-2021, 08:51:56', 'siswa'),
-(22, 'nana', 'nana@gmail.com', '518d5f3401534f5c6c21977f12f60989', '10-07-2021, 17:16:00', 'siswa'),
 (23, 'admin', 'admin@gmail.com', '21232f297a57a5a743894a0e4a801fc3', '10-07-2021, 17:17:39', 'admin'),
-(24, 'mama', 'mama@gmail.com', 'eeafbf4d9b3957b139da7b7f2e7f2d4a', '10-07-2021, 21:35:54', 'siswa');
+(28, 'lala', 'lala@gmail.com', '2e3817293fc275dbee74bd71ce6eb056', '20-07-2021, 17:36:19', 'siswa');
 
 --
 -- Indexes for dumped tables
@@ -247,31 +242,31 @@ ALTER TABLE `table_jadwal`
 -- AUTO_INCREMENT for table `table_nilai`
 --
 ALTER TABLE `table_nilai`
-  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_nilai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `table_saran`
 --
 ALTER TABLE `table_saran`
-  MODIFY `id_saran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_saran` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `table_sekolah`
 --
 ALTER TABLE `table_sekolah`
-  MODIFY `id_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id_sekolah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `table_siswa`
 --
 ALTER TABLE `table_siswa`
-  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `table_user`
 --
 ALTER TABLE `table_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
